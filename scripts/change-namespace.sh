6bc171fcf50af6049147ce416326e1bda534502e
TuxInvader <github@badpenguin.co.uk>
2023-01-29 11:19:57 +0000

    ns change script

---
+++ scripts/change-namespace.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+gitNS=nginxinc
+newNS=tuxinvader
+dirs="galaxy.yml plugins roles docs README*"
+
+if [ ! -d .git ]
+then
+    echo "This script should be run from the root directory of the source tree"
+    exit 1
+fi
+
+case $1 in
+  from-git)
+    find $dirs -type f -exec sed -i -re "s/${gitNS}/${newNS}/g" {} \;
+    ;;
+  to-git)
+    find $dirs -type f -exec sed -i -re "s/${newNS}/${gitNS}/g" {} \;
+    ;;
+  *)
+    echo "Usage: $0 [to-git|from-git]"
+esac
+
